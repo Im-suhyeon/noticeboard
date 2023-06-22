a686baeaad2f124d45115429e614596c359a1286
Im-suhyeon <00suhyen@gmail.com>
2023-06-23 03:22:57 +0900

    230623 p110 완료

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //롬복의 어노테이션
+@NoArgsConstructor //롬복의 어노테이션
+@Entity //JPA의 어노테이션, Entity 클래스에서는 절대 Setter 메소드를 만들지 않는다
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
